b532e9ddfd75d5a29dba7f3945e75599d17eae19
Finest <47058692+finestwebdesign@users.noreply.github.com>
2022-05-10 16:09:17 +0300

    news

--- script/sidebar.js
+++ script/sidebar.js
@@ -4,4 +4,6 @@ $('.nav-toggle').click(function(e) {
     $("html").toggleClass("openNav");
     $(".nav-toggle").toggleClass("active");
     $(".phone_menu").toggleClass("hidden");
+    setTimeout(() => {$(".menu_button").toggleClass("hidden_display"); $(".social_button").toggleClass("hidden_display"); }, 50);
+    
   });
