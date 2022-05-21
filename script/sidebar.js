b2c9e260f215e4cb568ff07c63bd5fcce8feaa1b
finestwebdesign <deathkv2.0@gmail.com>
2022-05-21 16:09:35 +0300

    new sidebar

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,7 +1,13 @@
-$('.nav-toggle').click(function(e) {
-  
-    e.preventDefault();
-    $("html").toggleClass("openNav");
-    $(".nav-toggle").toggleClass("active");
-    $(".phone_menu").toggleClass("hidden");
-  });
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("hamburger_opener").style.display = "none"
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("hamburger_opener").style.display = "flex"
+}
